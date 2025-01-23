ebfd0cacb9e0bb6dba99695df051751002dde55e
Wai Kyaw Myo <waikyawmyo4u@gmail.com>
2025-01-23 22:33:19 +0700

    Simple server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 
 const fs = require('fs')
-
+const http= require('http')
 // // //Blocking, synchronous way
 // const textIn =fs.readFileSync('./txt/input.txt','utf-8')
 // const Textout  = `this is what we know about the avocod ${textIn}.`
@@ -8,9 +8,19 @@ const fs = require('fs')
 // console.log('File written')
 
 //Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data)=>{
-        console.log(data)
-    })
+// fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data)=>{
+//         console.log(data)
+//     })
+// })
+// console.log("will read file!")
+
+//SERVER
+const server = http.createServer((req,res)=>{
+    console.log(req)
+    res.end("HEllo Form the server!")
+}) 
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log("LIsting to requirests on port 8000")
 })
-console.log("will read file!")
