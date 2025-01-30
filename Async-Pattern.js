b510d5bdffa7d1b78bb9860285ba910a6499d496
Wai Kyaw Myo <waikyawmyo4u@gmail.com>
2025-01-30 17:30:44 +0700

    Node Async Native Option

--- Async-Pattern.js
+++ Async-Pattern.js
@@ -1,25 +1,37 @@
-const {readFile}= require('fs')
+const {readFile,writeFile}= require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const getText = (path)=>{
-    return new Promise((resolve,reject)=>{
-        readFile(path,'utf8',(err,data)=>{
-            if(err){
-                reject(err)
-            }else {
-                resolve(data)
-            }
-        })
-    })
-}   
-// getText('./text-file.txt').then(result=>console.log(result)).catch((err)=>console.log(err))
-//Reduce
 const start = async()=>{
     try{
-        const first = await getText('./text-file.txt')
-        console.log(first)
+        const first = await readFile('./text-file.txt','utf8')
+        const second = await readFile('./','utf8')
+        await writeFile(
+            './text-file.txt',
+            `This IS Awesone : ${first} ${second}`,
+            {flag:"a"}
+        )
+        console.log(first,second)
     }catch(error){
         console.log(error)
     }
 }
+
+
+// const getText = (path)=>{
+//     return new Promise((resolve,reject)=>{
+//         readFile(path,'utf8',(err,data)=>{
+//             if(err){
+//                 reject(err)
+//             }else {
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }   
+// getText('./text-file.txt').then(result=>console.log(result)).catch((err)=>console.log(err))
+//Reduce
+
 
 start()
