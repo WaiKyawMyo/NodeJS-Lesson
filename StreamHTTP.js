7d072207b19518a263689f0f65358fafc67fe624
Wai Kyaw Myo <waikyawmyo4u@gmail.com>
2025-02-01 21:08:06 +0700

    Strream

---
+++ StreamHTTP.js
@@ -0,0 +1,12 @@
+var http = require('http')
+var fs= require('fs')
+
+http.createServer(function (req,res){
+    const fileStream= fs.createReadStream("./","utf8")
+    fileStream.on('open',()=>{
+        fileStream.pipe(res)
+    })
+    fileStream.on('error',(err)=>{
+        res.end(err)
+    })
+}).listen(5000)
