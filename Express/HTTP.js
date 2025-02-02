3cad960948cd2eb4b5fb91e060c09a3bc9c8cccd
Wai Kyaw Myo <waikyawmyo4u@gmail.com>
2025-02-02 18:20:14 +0700

    HTTP Request

---
+++ Express/HTTP.js
@@ -0,0 +1,24 @@
+const http = require('http')
+
+const server = http.cereateServer((req,res)=>{
+    const url= req.url
+    //Home page
+    if(url ==="/"){
+        res.writeHead(200,{'content-type': 'text/html'})
+        res.write('<h1>Home page</h1>')
+        res.end()
+    }//About page
+    else if(url==='/about'){
+        res.writeHead(200,{'content-type': 'text/html'})
+        res.write('<h1>About page</h1>')
+        res.end()
+    }//404
+    else{
+        res.writeHead(200,{'content-type': 'text/html'})
+        res.write('<h1>page not found</h1>')
+        res.end()
+    }
+    
+})
+
+server.listen(5000)
